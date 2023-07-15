b159e30c4ee715775ebe412400f2ae25b9a57252
prizey452 <89627967+prizey452@users.noreply.github.com>
2023-07-15 14:25:23 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,6 +7,6 @@ $database = ""; //enter here the name of your database
 $connection = mysqli_connect("$host", $username, $password, $database);
 if (mysqli_connect_errno()) {
     echo "CONNECTION ERROR" . mysqli_connect_error();
-    echo "alert('Check if you have entered an existing database name in connection.php file')";
+    echo "<script>alert('Check if you have entered an existing database name in connection.php file')</script>";
 }
 ?>
